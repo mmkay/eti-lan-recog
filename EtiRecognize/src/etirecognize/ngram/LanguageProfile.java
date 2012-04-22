be298d183fa68aead5baf52659e9f0ae8503272c
Mateusz Kulewicz <matikula9@gmail.com>
2012-04-22 13:12:24 +0200

    Działające porównanie języków, potrzebne jeszcze więcej próbek do sprawdzenia poprawności

--- EtiRecognize/src/etirecognize/ngram/LanguageProfile.java
+++ EtiRecognize/src/etirecognize/ngram/LanguageProfile.java
@@ -8,37 +8,35 @@ import java.util.TreeSet;
 
 /**
  * LanguageProfile
- * 
- * Class to contain a profile of language or a document, basing on ngrams
- * it contains the frequency of all 1- to 5-grams basing on
- * a specific data set
- * 
- * Based on
- * http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.53.9367
- * 
+ *
+ * Class to contain a profile of language or a document, basing on ngrams it
+ * contains the frequency of all 1- to 5-grams basing on a specific data set
+ *
+ * Based on http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.53.9367
+ *
  * @author Mateusz Kulewicz
  */
 public class LanguageProfile {
     //language name
+
     private final String name;
     //array of 1 to 5-grams - not sure which collection I should use to get
     //a good comparison
-    private HashMap<String, Integer>[] ngram;
-    private TreeSet<NGramEntity>[] sortedNGrams;
-    public LinkedList<String>[] order;
+    private HashMap<String, Integer> ngram;
+    private TreeSet<NGramEntity> sortedNGrams;
+    public LinkedList<String> order;
 
     public LanguageProfile(String name) {
         this.name = name;
-        ngram = new HashMap[5];
-        sortedNGrams = new TreeSet[5];
-        order = new LinkedList[5];
-        for (int i = 0; i < 5; i++) {
-            ngram[i] = new HashMap<>();
-            sortedNGrams[i] = new TreeSet<>();
-            order[i] = new LinkedList<>();
-        }
+        ngram = new HashMap<>();
+        sortedNGrams = new TreeSet<>();
+        order = new LinkedList<>();
     }
-    
+
+    public String getName() {
+        return name;
+    }
+
     /*
      * add data from file
      */
@@ -57,7 +55,7 @@ public class LanguageProfile {
             //System.out.println(text);
             contents.append(text);
         }
-        
+
         //divide buffer into words
         String buffer = contents.toString();
         String[] words = buffer.split("\\s");
@@ -69,39 +67,37 @@ public class LanguageProfile {
             for (int i = 1; i <= 5; i++) {
                 // i-gram
                 for (int j = 0; j <= word.length() - i; j++) {
-                    String key = word.substring(j, j+i);
-                    if (ngram[i-1].containsKey(key)) {
-                        ngram[i-1].put(key, ngram[i-1].get(key) + 1);
-                    }
-                    else {
-                        ngram[i-1].put(key, 1);
+                    String key = word.substring(j, j + i);
+                    if (ngram.containsKey(key)) {
+                        ngram.put(key, ngram.get(key) + 1);
+                    } else {
+                        ngram.put(key, 1);
                     }
                 }
             }
         }
         //at this point, we've got a HashMap of keys and their values
-        
-        for (int i = 0; i < 5; i++) {
-            Set<String> keys;
-            keys = ngram[i].keySet();
-            for (String key : keys) {
-                NGramEntity entity = new NGramEntity(key);
-                entity.setOccurences(ngram[i].get(key));
-                boolean check = sortedNGrams[i].add(entity);
-            }
+
+        Set<String> keys;
+        keys = ngram.keySet();
+        for (String key : keys) {
+            NGramEntity entity = new NGramEntity(key);
+            entity.setOccurences(ngram.get(key));
+            boolean check = sortedNGrams.add(entity);
         }
-        
-        
+
+
         //NGramEntities are now sorted into the TreeSets, so we've got a profile
         //it might be useful to simply put names into the LinkedList
-        
-        for (int i = 0; i < 5; i++) {
-            NGramEntity element = sortedNGrams[i].last();
-            do {
-                String name = element.getSequence();
-                order[i].add(name);
-            } while ((element = sortedNGrams[i].lower(element)) != null );
-        }
+        //we only copy first 300 n-grams
+
+        NGramEntity element = sortedNGrams.last();
+        int counter = 0;
+        do {
+            String elname = element.getSequence();
+            order.add(elname);
+            counter++;
+        } while ((element = sortedNGrams.lower(element)) != null && counter <= 300);
+
     }
-    
 }
--- EtiRecognize/src/etirecognize/ngram/MainComparator.java
+++ EtiRecognize/src/etirecognize/ngram/MainComparator.java
@@ -3,19 +3,21 @@ package etirecognize.ngram;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Vector;
 
 /**
  * MainComparator - class that compares language profiles with given samples
  * @author mat
  */
 public class MainComparator {
+
+    static String[] langs = {"eng", "pol"};
+    static String[] checked = {"eng-nasa", "eng-hero", "pol-wiki",
+        "eng-oneword", "eng-wiki"};
     
-    //TODO: comparation of profiles
-    
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        /*
+         * Use example:
         LanguageProfile prof1 = new LanguageProfile("eng");
         try {
             prof1.addData(new File("./src/etirecognize/ngram/samples/eng.txt"));
@@ -24,8 +26,54 @@ public class MainComparator {
         } catch (IOException ex) {
             Logger.getLogger(MainComparator.class.getName()).log(Level.SEVERE, null, ex);
         }
-        for (int i = 0; i < 5; i++) {
-            System.out.println((i+1)+"-grams ("+prof1.order[i].size()+"):");
+        */
+        Vector<LanguageProfile> languages = new Vector<LanguageProfile>();
+        Vector<LanguageProfile> tocheck = new Vector<LanguageProfile>();
+        
+        //create profiles of languages and samples to check
+        for (String lang : langs) {
+            LanguageProfile prof = new LanguageProfile(lang);
+            prof.addData(new File("./src/etirecognize/ngram/samples/"+lang+".txt"));
+            languages.add(prof);
         }
+        for (String lang : checked) {
+            LanguageProfile prof = new LanguageProfile(lang);
+            prof.addData(new File("./src/etirecognize/ngram/samples/"+lang+".txt"));
+            tocheck.add(prof);
+        }
+        
+        //sample check
+        for (int i = 0; i < tocheck.size(); i++) {
+            System.out.println("Checking sample "+tocheck.get(i).getName());
+            int dist = 100000;
+            String langname = "";
+            for (int j = 0; j < languages.size(); j++) {
+                int temp = calculateDistance(languages.get(j), tocheck.get(i));
+                if (temp < dist) {
+                    dist = temp;
+                    langname = languages.get(j).getName();
+                }
+                System.out.println("Language "+languages.get(j).getName()+": "+temp);
+            }
+            System.out.println("Detected language: "+langname+", distance: "+dist);
+        }
+    }
+    
+    /* Method that calculates the distance between a language "lang" and
+     * a sample we are trying to identify the language of.
+     */
+    public static int calculateDistance(LanguageProfile lang, LanguageProfile sample) {
+        int result = 0;
+        
+        for (String key : sample.order) {
+            if (lang.order.contains(key)) {
+                result += java.lang.Math.abs(lang.order.indexOf(key) - sample.order.indexOf(key));
+            }
+            else {
+                result += lang.order.size();
+            }
+        }
+        
+        return result;
     }
 }
